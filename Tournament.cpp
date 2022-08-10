75a04cae0af1c211e139fa3f49ee05756fadb302
qirunlin <88021442+qirunlin@users.noreply.github.com>
2022-08-10 15:58:21 +0930

    hello

--- Tournament.cpp
+++ Tournament.cpp
@@ -15,27 +15,32 @@ Player* Tournament ::run(std::array<Player*, 8>competitors){
     Referee r1;
     std::array<Player*, 4>competitors2;
     std::array<Player*, 2>competitors3;
-
+int w=0;
+int w2=0;
     for(int i=0;i<7;i=i+2){
-        r1.gameref(competitors.at(i),competitors.at(i+1));
-        for(int w=0;w<4;w++){
+       
+       
+       
+            r1.gameref(competitors.at(i),competitors.at(i+1));
             competitors2.at(w)=r1.winner;
-        }
+    w++; 
 
     }
 
     for(int g2=0;g2<3;g2=g2+2){
+        
         r1.gameref(competitors2.at(g2),competitors2.at(g2+1));
-        for(int w2=0;w2<2;w2++){
+       
         competitors3.at(w2)=r1.winner;
-        }
+        w2++;
 
     }
 
      r1.gameref(competitors3.at(0),competitors3.at(1));
 
 
-     cout<<&r1.winner->playerid;
+     //cout<<&r1.winner->playerid;
+     //cout<<r1.winner;
      return r1.winner;
 
     
