a0a0680f6edfb84e4098c16a14ea6e1dc249d9d0
qirunlin <88021442+qirunlin@users.noreply.github.com>
2022-10-21 14:56:35 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "linkedlist.h"
 #include<iostream>
+#include <vector>;
 using namespace std;
 int main(){
     linkedlsit l1;
+int x;
+    string a1[10]={};
+    for(int i=0;i<100;i++){
+        cin>>a1[i];
+        x++;
+    }
 
-    string a1[5]={"*","-","5","6","7"};
-
-    l1.ptop(a1,5);
+    l1.ptop(a1,x);
 }
